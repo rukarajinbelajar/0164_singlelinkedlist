8961c3c507df8a0cea4f4be1cc8380f865a19f5b
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-24 15:36:53 +0700

    menambahkan kode

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -34,11 +34,26 @@ public:
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
 
             Node *previos = START;
             Node *current =START;
 
-            
-        }
+            while ((current != NULL) && (nim >= current->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+    
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
+
+    
 };
