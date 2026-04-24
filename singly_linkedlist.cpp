950ee37f2cb03d4f2874294ee5658be0415ba153
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-24 15:53:06 +0700

    membuat program traverse

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -81,10 +81,32 @@ public:
         Node *current, *previous;
         if (!Search(nim, &previous, &current))
             return false;
+
+        if (current == START)
+            START = START->next;
         else
             previous->next = current->next;
         
         delete current;
         return true;
+    }
+
+    void traverse ()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
     }
 };
